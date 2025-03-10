6e0cbc8d35ec7bc5a1feeb0311997f96cd47ca8e
Yuzgin <122491012+Yuzgin@users.noreply.github.com>
2025-03-10 16:34:40 +0000

    Reducing CUDA load

--- transformerModel.py
+++ transformerModel.py
@@ -76,8 +76,8 @@ def main():
     train_dataset = ChestXrayDataset(data_folder, csv_file, transform_train, train_list)
     test_dataset = ChestXrayDataset(data_folder, csv_file, transform_test, test_list)
 
-    train_loader = DataLoader(train_dataset, batch_size=16, shuffle=True, num_workers=2)
-    test_loader = DataLoader(test_dataset, batch_size=16, shuffle=False, num_workers=2)
+    train_loader = DataLoader(train_dataset, batch_size=8, shuffle=True, num_workers=2)
+    test_loader = DataLoader(test_dataset, batch_size=8, shuffle=False, num_workers=2)
 
     # Load pretrained Vision Transformer
     weights = ViT_B_16_Weights.IMAGENET1K_V1
@@ -92,6 +92,8 @@ def main():
 
     criterion = nn.BCEWithLogitsLoss()
     optimizer = torch.optim.AdamW(model.parameters(), lr=5e-5)
+
+    torch.cuda.empty_cache()
 
     num_epochs = 5
     for epoch in range(num_epochs):
